9a3d1d78f65b66296abf7fd309f9a717d9781d26
eduardoagas <eagasilva12@gmail.com>
2025-07-23 03:11:06 -0300

    hiteffects and singleton

--- Assets/Scripts/Mechanics/HitEffectManager.cs
+++ Assets/Scripts/Mechanics/HitEffectManager.cs
@@ -4,6 +4,14 @@ public class HitEffectManager : MonoBehaviour
 {
     public static HitEffectManager Instance { get; private set; }
 
+    [Header("World Effects")]
+    public GameObject worldHitEffectPrefab;
+    public AudioClip hitSound;
+
+    [Header("UI Effects")]
+    public GameObject uiHitEffectPrefab;
+    public Canvas uiCanvas;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -15,18 +23,29 @@ public class HitEffectManager : MonoBehaviour
         Instance = this;
     }
 
-    public void ApplyHitEffect(Vector3 worldPosition, WeaponEffectData weaponEffect)
+    public void ApplyWorldHitEffect(Vector3 position)
     {
-        if (weaponEffect == null) return;
+        if (worldHitEffectPrefab != null)
+            Instantiate(worldHitEffectPrefab, position, Quaternion.identity);
 
-        if (weaponEffect.hitEffectPrefab != null)
-        {
-            Instantiate(weaponEffect.hitEffectPrefab, worldPosition, Quaternion.identity);
-        }
+        if (hitSound != null)
+            AudioSource.PlayClipAtPoint(hitSound, position);
+    }
 
-        if (weaponEffect.hitSound != null)
-        {
-            AudioSource.PlayClipAtPoint(weaponEffect.hitSound, worldPosition);
-        }
+    public void ApplyUIHitEffect(Vector3 screenPosition)
+    {
+        if (uiCanvas == null || uiHitEffectPrefab == null)
+            return;
+
+        Vector2 uiPosition;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            uiCanvas.transform as RectTransform,
+            screenPosition,
+            uiCanvas.worldCamera,
+            out uiPosition
+        );
+
+        GameObject effect = Instantiate(uiHitEffectPrefab, uiCanvas.transform);
+        effect.GetComponent<RectTransform>().anchoredPosition = uiPosition;
     }
 }
